1677790382c0d22fcb3d3bc76e4445cf37f336f9
Oriana Nohemi Reina Marcano <oriana.nohemi22@gmail.com>
2022-12-04 22:11:34 -0500

    Contacto (#2)

---
+++ app/components/selections/scripts/Contacto.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function() {
+    this.document.getElementById("btnSubmit").addEventListener("click", function() {
+        alert("Mensaje enviado con éxito");
+    })
+})
